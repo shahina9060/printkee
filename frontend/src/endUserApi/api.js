835c8416c1e50852c7b0b76c118144eb41373ddd
shahina9060 <141648831+shahina9060@users.noreply.github.com>
2025-07-09 16:43:15 +0530

    Update api.js

--- frontend/src/endUserApi/api.js
+++ frontend/src/endUserApi/api.js
@@ -1,11 +1,11 @@
-// export const backendEndApi = "http://localhost:5050/api"
+export const backendEndApi = "http://localhost:5050/api"
 export const backendEndApi = "https://printkee-backend.onrender.com/api"
 // export const backendEndApi = "http://69.62.75.132:5050/api"
 // export const backendEndApiBannerUpload = "http://localhost:5050"
 
-// export const backendEndApiBannerUpload = "https://printkee-backend.onrender.com"
+export const backendEndApiBannerUpload = "https://printkee-backend.onrender.com"
 
-export const backendEndApiBannerUpload = "https://printkee-backend.onrender.com/uploads/images/";
+// export const backendEndApiBannerUpload = "https://printkee-backend.onrender.com/uploads/images/";
 
 
 
